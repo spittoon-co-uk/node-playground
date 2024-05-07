8e5033701a1fe400f8dfafd5a7857ee60c62ba27
Michael Adamson <michael.j.adamson@virginmeida.com>
2024-05-07 19:01:25 +0100

    some super-basic games with values/logical operators/variable type

--- eloquent-javascript/part-1/run.js
+++ eloquent-javascript/part-1/run.js
@@ -1,5 +1,14 @@
-let int = 8;
-let multiplier = 13;
-
-
-console.log('our multiplied int: ',int * multiplier);
+let x = 10;
+let y = 3;
+let z = x % y;
+console.log(z);
+console.log(`this a backtick-quoted string with a ${z * 100} variable calculated on-the-fly`);
+console.log(typeof `this a backtick-quoted string with a ${z * 100} variable`);
+console.log(8 * null);
+console.log("5" - 1);
+console.log("five" + 2);
+console.log(false == 0);
+console.log(null || "user"); // This is how we fall back when the left-hand value evaluates as falsey
+console.log("Agnes" || "user");
+console.log(true ? "evaluated to true" : "evaluated to false");
+console.log(y ? y : "evaluated to false");
